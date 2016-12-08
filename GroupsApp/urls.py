1c91bed22ffcaa8c8ec2cffc523dcf653846c1b8
Justin Alwine <alwine@Purdue.edu>
2016-12-08 15:27:23 -0500

    Added ability to remove a group, currently anyone can remove a group

--- GroupsApp/urls.py
+++ GroupsApp/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     url(r'^group/formsuccess$', views.getGroupFormSuccess, name='GroupFormSuccess'),
     url(r'^group/join$', views.joinGroup, name='GJoin'),
     url(r'^group/unjoin$', views.unjoinGroup, name='GUnjoin'),
+    url(r'^group/remove$', views.removeGroup, name='GRemove'),
     url(r'^group$', views.getGroup, name='Group'),
 ]
